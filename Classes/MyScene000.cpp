87d0b2072413e3a171aff50f4b4844a921b5609b
wangzhe <lancocos@gmail.com>
2019-09-15 16:32:48 +0800

    1

--- Classes/MyScene000.cpp
+++ Classes/MyScene000.cpp
@@ -26,11 +26,33 @@ bool MyScene000::init() {
 	auto sp2 = Sprite::create("i1.png", Rect(0, 0, 50, 50));
 	sp2->setPosition(Vec2(150, 100));
 	this->addChild(sp2);
+	auto spritecache = SpriteFrameCache::getInstance();
+	spritecache->addSpriteFramesWithFile("f1.plist");
+	//auto ss1 = Sprite::createWithSpriteFrameName("i1.png");
+	//ss1->setPosition(Vec2(200, 100));
+	//this->addChild(ss1);
+	auto pinfo = AutoPolygon::generatePolygon("i1.png");
+	auto ss1 = Sprite::create(pinfo);
+	ss1->setPosition(Vec2(200, 100));
+	this->addChild(ss1);
+	auto mt = MoveTo::create(10, Vec2(200, 200));
+	ss1->runAction(mt);
+	auto myLabel = Label::createWithBMFont("markerFelt.fnt", "Your Text");
+	myLabel->setPosition(Vec2(150, 200));
+	myLabel->enableGlow(Color4B::YELLOW);
+	this->addChild(myLabel);
+	auto func = []() {CCLOG("ADSD");};
+	auto label1 = LabelTTF::create("stop","Arial",14);
+	auto mux = MenuItemLabel::create(label1);
+	auto mmm = Menu::create(mux, NULL);
+	mmm->setPosition(Vec2(300, 200));
+	this->addChild(mmm);
+
 	return true;
 }
 void MyScene000::toScene(cocos2d::Ref* pSender) {
 	CCLOG("CLOSE ITEM");
 	auto director = Director::getInstance();
 	auto s = MyScene000::create();
-	//Director::getInstance()->replaceScene(s);
+	Director::getInstance()->replaceScene(TransitionFlipX::create(2, s));
 }
